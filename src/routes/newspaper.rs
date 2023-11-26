b60c3a4903152a7d7672b5bd8578695480848355
nyx <loool1912@web.de>
2023-11-26 23:37:47 +0100

    added article feed to newspaper

--- src/routes/newspaper.rs
+++ src/routes/newspaper.rs
@@ -16,13 +16,24 @@ use crate::common::tools::format_date;
 
 use super::{AppState, UserData};
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct ArticlePreview {
+    id: i64,
+    title: String,
+    upvote_count: i64,
+    publish_date: String,
+}
+
 #[derive(Template)]
 #[template(path = "newspaper/view.html")]
 struct NewspaperTemplate {
     newspaper_name: String,
     newspaper_avatar: String,
     newspaper_created_at: String,
-    //todo: add article feed of a newspaper
+    articles: Vec<ArticlePreview>,
+    owner_id: i64,
+    owner_name: String,
+    owner_avatar: String,
 }
 
 async fn newspaper(
@@ -44,10 +55,48 @@ async fn newspaper(
         user_message: format!("No newspaper with id {newspaper_id} was found."),
     })?;
 
+    let articles: Vec<ArticlePreview> = query!(
+        r#"SELECT articles.id,
+        articles.title,
+        articles.created_at,
+        COALESCE(uv.upvote_count,0) AS upvote_count
+ FROM articles
+
+          LEFT JOIN (SELECT article_id, count(*) upvote_count
+                     FROM upvotes
+                     GROUP BY article_id) as uv
+                    ON uv.article_id = articles.id
+                    where articles.newspaper_id = $1
+ ORDER BY articles.created_at DESC
+ LIMIT 30;"#,
+        &newspaper_id
+    )
+    .fetch_all(&db_pool)
+    .await?
+    .iter()
+    .map(|a| ArticlePreview {
+        id: a.id,
+        title: a.title.clone(),
+        publish_date: format_date(a.created_at),
+        upvote_count: a.upvote_count.unwrap(),
+    })
+    .collect();
+
+    let newspaper_owner =  query!(
+        r#"SELECT id, name, avatar FROM users WHERE id in (select user_id from journalists where rank = 'owner' and newspaper_id = $1 )"#,
+        newspaper_id
+    )
+    .fetch_one(&db_pool)
+    .await?;
+
     Ok(NewspaperTemplate {
         newspaper_name: newspaper.name,
         newspaper_avatar: newspaper.avatar,
         newspaper_created_at: format_date(newspaper.created_at),
+        articles: articles,
+        owner_id: newspaper_owner.id,
+        owner_name: newspaper_owner.name.unwrap(),
+        owner_avatar: newspaper_owner.avatar.unwrap_or("".to_owned()),
     })
 }
 
